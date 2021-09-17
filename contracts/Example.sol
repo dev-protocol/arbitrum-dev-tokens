f18efa1c4b5539001860ac69c7b1d1e4e7d830c4
Stuart <11183054+stuartwk@users.noreply.github.com>
2021-09-17 14:49:50 +0900

    Initial commit

---
+++ contracts/Example.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity =0.8.7;
+
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Example is Initializable {
+	using SafeMath for uint256;
+	uint256 public value;
+
+	function initialize(uint256 initialValue) external initializer {
+		value = initialValue;
+	}
+
+	function add(uint256 v) external {
+		value = value.add(v);
+	}
+}

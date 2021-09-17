6bca42d37df76b8cd77af8ff834369e35b2f4cef
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-09-17 16:08:30 +0900

    adds comments

--- contracts/Dev.sol
+++ contracts/Dev.sol
@@ -5,29 +5,47 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "./interface/IArbToken.sol";
 
+/// @title Upgradeable Arbitrum-compliant DEV Token
 contract DEV is Initializable, ERC20Upgradeable, IArbToken {
-
 	address public l2Gateway;
-    address public override l1Address;
+	address public override l1Address;
 
 	modifier onlyGateway() {
-        require(msg.sender == l2Gateway, "ONLY_l2GATEWAY");
-        _;
-    }
+		require(msg.sender == l2Gateway, "ONLY_l2GATEWAY");
+		_;
+	}
 
-	function initialize(uint256 initialSupply, address _l2Gateway, address _l1Address) public initializer {
+	/// Standard OZ upgradeable initializer
+	function initialize(
+		uint256 initialSupply,
+		address _l2Gateway,
+		address _l1Address
+	) public initializer {
 		__ERC20_init("Dev", "DEV");
-        _mint(_msgSender(), initialSupply);
+		_mint(_msgSender(), initialSupply);
 		l1Address = _l1Address;
 		l2Gateway = _l2Gateway;
 	}
 
-	function bridgeMint(address account, uint256 amount) external override onlyGateway {
+	/// Mint new DEV tokens
+	/// @param account is the address to send the newly minted tokens
+	/// @param amount to mint
+	function bridgeMint(address account, uint256 amount)
+		external
+		override
+		onlyGateway
+	{
 		_mint(account, amount);
 	}
 
-	function bridgeBurn(address account, uint256 amount) external override onlyGateway {
+	/// Burn DEV tokens
+	/// @param account is the address to burn the tokens
+	/// @param amount to burn
+	function bridgeBurn(address account, uint256 amount)
+		external
+		override
+		onlyGateway
+	{
 		_burn(account, amount);
 	}
-
 }
--- contracts/interface/IArbToken.sol
+++ contracts/interface/IArbToken.sol
@@ -2,18 +2,18 @@
 pragma solidity =0.8.7;
 
 interface IArbToken {
-    /**
-     * @notice should increase token supply by amount, and should (probably) only be callable by the L1 bridge.
-     */
-    function bridgeMint(address account, uint256 amount) external;
+	/**
+	 * @notice should increase token supply by amount, and should (probably) only be callable by the L1 bridge.
+	 */
+	function bridgeMint(address account, uint256 amount) external;
 
-    /**
-     * @notice should decrease token supply by amount, and should (probably) only be callable by the L1 bridge.
-     */
-    function bridgeBurn(address account, uint256 amount) external;
+	/**
+	 * @notice should decrease token supply by amount, and should (probably) only be callable by the L1 bridge.
+	 */
+	function bridgeBurn(address account, uint256 amount) external;
 
-    /**
-     * @return address of layer 1 token
-     */
-    function l1Address() external view returns (address);
+	/**
+	 * @return address of layer 1 token
+	 */
+	function l1Address() external view returns (address);
 }
